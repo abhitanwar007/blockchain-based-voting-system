05c0fa30c31976ec957934a4cfc3a9faa431102f
ABHISHEK <76835879+abhitanwar007@users.noreply.github.com>
2023-04-20 07:40:15 +0530

    Update VoteHelper.java

--- src/voting/voter/VoteHelper.java
+++ src/voting/voter/VoteHelper.java
@@ -1,19 +1,4 @@
-/*
- * Copyright (C) 2019 nisid
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
+
 package voting.voter;
 
 import java.awt.Toolkit;
@@ -25,10 +10,6 @@ import javax.swing.JOptionPane;
 import org.web3j.protocol.core.methods.response.TransactionReceipt;
 import voting.ballot.Ballot;
 
-/**
- *
- * @author nisid
- */
 public class VoteHelper extends javax.swing.JFrame {
 
 	private final Ballot ballot;
